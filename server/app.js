4d93b1a668fe27aec80ffb55b5678320276c65ba
jkim8 <juno0218@naver.com>
2021-09-01 08:58:16 -0400

    router setting

--- server/app.js
+++ server/app.js
@@ -1,7 +1,32 @@
 import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import hpp from 'hpp'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
+import config from './config'
 
 const app = express()
+const { MONGO_URI } = config
 
+app.use(hpp())
+app.use(helmet())
+
+app.use(cors({ origin: true, credential: true }))
+app.use(morgan("dev"))
+
+app.use(express.json())
+
+mongoose.connect(MONGO_URI, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+})
+    .then(() => console.log("MongoDB connecting Success!!"))
+    .catch((e) => console.log(e))
+
+//Use routes    
 app.get('/')
 
 export default app
