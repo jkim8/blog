fc72c86138565d60bbfc47bd42f4dc8835375015
jkim8 <juno0218@naver.com>
2021-09-10 14:20:15 -0400

    redux

---
+++ client/src/redux/sagas/index.js
@@ -0,0 +1,5 @@
+import { all } from 'redux-saga'
+
+export default function* rootSaga() {
+    yield all([])
+}
---
+++ client/src/store.js
@@ -0,0 +1,29 @@
+import { createStore, compose, applyMiddleware } from "redux"
+import createSagaMiddleware from "redux-saga"
+import { createBrowserHistory } from "history"
+import { routerMiddleware } from "connected-react-router"
+
+import createRootReducer from "./redux/reducers"
+import rootSaga from "./redux/sagas"
+
+export const history = createBrowserHistory()
+
+const createSagaMiddleware = createSagaMiddleware()
+
+const initialState = {}
+
+const middlewares = [createSagaMiddleware, routerMiddleware(history)]
+const devtools = window.EXTENSION_COMPOSE__
+
+const composeEnhancer =
+    process.env.NODE_ENV === "production" ? compose : devtools || compose
+
+const store = createStore(
+    createRootReducer(history),
+    initialState,
+    composeEnhancer(applyMiddleware(...middlewares))
+
+)
+sagaMiddleware.run(rootSaga)
+
+export default store
