5d248fb26f658f27e814c1dab00f9049b60b3ae6
jkim8 <juno0218@naver.com>
2021-09-15 08:19:44 -0400

    header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Col, Row } from "reactstrap"
+
 
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <div id="page-header" className="mb-3">
+            <Row>
+                <Col md="6" sm="auto" className="text-center m-auto">
+                    <h1>Read Our Blog</h1>
+                    <p>Jeff의 사이드 프로젝트 작업블로그 입니다.</p>
+                </Col>
+            </Row>
+        </div>
+    )
 
 }
 
